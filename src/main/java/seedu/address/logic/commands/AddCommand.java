59319fb492aa6811b39aa2b94ce09619f60b7dcc
CalistaIo <e0426281@u.nus.edu>
2020-10-14 14:41:52 +0800

    Edit ui messages again

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -16,18 +16,18 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a flashcard to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a flashcard to the flashcard list. "
             + "Parameters: "
             + PREFIX_TITLE + "TITLE "
             + PREFIX_DEFINITION + "ADDRESS "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_TITLE + "John Doe "
+            + PREFIX_TITLE + "Bobosort "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_DEFINITION + "<PREFIX_DEFINITION> placeholder "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_TAG + "sorting "
+            + PREFIX_TAG + "algorithm";
 
     public static final String MESSAGE_SUCCESS = "New flashcard added: %1$s";
     public static final String MESSAGE_DUPLICATE_FLASHCARD = "This flashcard already exists in the address book";
