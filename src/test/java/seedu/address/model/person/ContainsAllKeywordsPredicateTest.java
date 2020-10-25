9d4bb84ec17769e74f0bf64462c45361b089fdda
jialin7878 <cjl@u.nus.edu>
2020-10-25 23:33:20 +0800

    more tests for multiple keyword searches

--- src/test/java/seedu/address/model/person/ContainsAllKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/ContainsAllKeywordsPredicateTest.java
@@ -41,15 +41,45 @@ public class ContainsAllKeywordsPredicateTest {
     }
 
     @Test
-    public void test_nameContainsKeywords_returnsTrue() {
-        // One keyword
+    public void test_flashcardContainsOneKeyword_returnsTrue() {
+        // Name
         ContainsAllKeywordsPredicate predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
-                preparePredicate("name", "Alice")));
-        assertTrue(predicate.test(new FlashcardBuilder().withTitle("Alice Bob").build()));
+                preparePredicate("name", "Quicksort")));
+        assertTrue(predicate.test(new FlashcardBuilder().withTitle("Quicksort").build()));
+
+        // Tag
+        predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("tag", "heaps")));
+        assertTrue(predicate.test(new FlashcardBuilder().withTags("heaps").build()));
+
+        // Priority
+        predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("priority", "high")));
+        assertTrue(predicate.test(new FlashcardBuilder().withPriority("high").build()));
     }
 
     @Test
-    public void test_nameDoesNotContainKeywords_returnsFalse() {
+    public void test_flashcardContainsMultipleKeywords_returnsTrue() {
+        // Two keywords of same type (name and name)
+        ContainsAllKeywordsPredicate predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("name", "Breadth"), preparePredicate("name", "first")));
+        assertTrue(predicate.test(new FlashcardBuilder().withTitle("Breadth first search").build()));
+
+        // Two keywords of different type (name and tag)
+        predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("name", "Quicksort"), preparePredicate("tag", "Sorting")));
+        assertTrue(predicate.test(new FlashcardBuilder().withTitle("Quicksort").withTags("Sorting").build()));
+
+        // All three types of keywords
+        predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("name", "Quicksort"), preparePredicate("tag", "Sorting"),
+                preparePredicate("priority", "low")));
+        assertTrue(predicate.test(new FlashcardBuilder().withTitle("Quicksort").withTags("Sorting")
+                .withPriority("low").build()));
+    }
+
+    @Test
+    public void test_flashcardDoesNotContainKeyword_returnsFalse() {
         // Zero keywords
         ContainsAllKeywordsPredicate predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
                 preparePredicate("name", "")));
@@ -65,6 +95,19 @@ public class ContainsAllKeywordsPredicateTest {
                 preparePredicate("name", "Main")));
         assertFalse(predicate.test(new FlashcardBuilder().withTitle("Alice")
                 .withDefinition("Main Street").build()));
+    }
+
+    @Test
+    public void test_flashcardContainsSomeButNotAllKeywords_returnsFalse() {
+        // Matches one but not the other (same type: name and name)
+        ContainsAllKeywordsPredicate predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("name", "Alice"), preparePredicate("name", "Carol")));
+        assertFalse(predicate.test(new FlashcardBuilder().withTitle("Alice Bob").build()));
+
+        // Matches one but not the other (different types: name and tag)
+        predicate = new ContainsAllKeywordsPredicate(preparePredicateList(
+                preparePredicate("name", "Quicksort"), preparePredicate("tag", "heaps")));
+        assertFalse(predicate.test(new FlashcardBuilder().withTitle("Quicksort").withTags("sorting").build()));
     }
 
 
