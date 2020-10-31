c6cbff531bcf7330e10f5071c1650bdab98f9375
CalistaIo <e0426281@u.nus.edu>
2020-10-31 09:45:14 +0800

    Annotate

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -78,7 +78,7 @@ public class MainApp extends Application {
         ReadOnlyAddressBook initialData;
 
         try {
-            addressBookOptional = storage.readAddressBook();
+            addressBookOptional = storage.readAddressBook(); // Entry point for storage is here!
             if (!addressBookOptional.isPresent()) {
                 logger.info("Data file not found. Will be starting with a sample AddressBook");
             }
--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -55,7 +55,7 @@ public class LogicManager implements Logic {
         commandResult = command.execute(model);
 
         try {
-            storage.saveAddressBook(model.getAddressBook());
+            storage.saveAddressBook(model.getAddressBook()); // write to Json file here
         } catch (IOException ioe) {
             throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
         }
